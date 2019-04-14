589ceb04e0b2323f914d6d40e1256be03c2529fd
Matheus Santos <mattsantos.conce@usp.br>
2019-04-14 20:15:01 -0300

    Implementei as funções de fitting e de prediction, faltam testes

--- ep3.py
+++ ep3.py
@@ -1,12 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def logistic_fit(X, y, w = None, batch_size=None, learnig_rate=1e-2,
         num_iterations=1000, return_history=False):
-    return #value
+    hw = []
+    if w == None:
+        w = np.random.rand(1, X.shape[1] + 1)
+    if batch_size == None or batch_size > X.shape[0]:
+        batch_size = X.shape[0]
+
+   # Cross-Entropy 
+   X = np.hstack((np.ones((X.shape[0],X))))
+    for t in range(num_iterations):
+        Ein = 0
+        for i in range(batch_size):
+            Ein += np.log(1 + np.exp(-y[i] * np.dot(w, X[i:])))
+        vt = (1.0/batch_size)*Ein
+        hw.append(w)
+        w += learning_rate*(vt)
+    return w if return_history == False else w, hw
 
 def logistic_predict(X,w):
-    return #value
-
+    X.np.hstack((np.ones((X.shape[0],X))))
+    s = np.dot(X, w)
+    return np.exp(s) / (1 + np.exp(s))
 
 def run_tests():
+    return 0
+
+
 
 
 if __name__ == '__main__':
