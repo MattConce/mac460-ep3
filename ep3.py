b79c799c81655d04371e7eab4455b4feb3397fa8
Matheus Santos <mattsantos.conce@usp.br>
2019-04-15 21:10:10 -0300

    Separei os testes do ep3.py, colocando-os no util.py

--- ep3.py
+++ ep3.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from numpy.linalg import inv
 
 
 def logistic_fit(X, y, w = None, batch_size=None, learning_rate=1e-2,
@@ -17,7 +15,8 @@ def logistic_fit(X, y, w = None, batch_size=None, learning_rate=1e-2,
         e = 0.0
         for i in range(batch_size):
                     a = -y[i]*Xe[i]
-                    s = sigmoid(y[i] * np.dot(w,Xe[i]))
+                    b = y[i] * np.dot(w,Xe[i])
+                    s = 1.0 / (1.0 + np.exp(-b)) # sigmoid function 
                     e += s*a
         gt = (-1.0/batch_size)*e
         hw.append(w)
@@ -30,89 +29,9 @@ def logistic_fit(X, y, w = None, batch_size=None, learning_rate=1e-2,
 
 
 def logistic_predict(X,w):
-    Xe = np.insert(X,0,1.0)
-    s = np.dot(w, Xe)
-    return sigmoid(s)
-
-
-def sigmoid(s):
-    return 1.0 / (1.0 + np.exp(-s))
-
-
-def run_tests1():
-    # Teste para pontos em duas dimensões
-    N = 1000
-
-    mean_pos = [0, 0]
-    mean_neg = [4, 0]
-    cov = [
-    [1, 0],
-    [0, 1]
-    ]
-
-    # Exemplos positivos
-    X_pos = np.random.multivariate_normal(mean_pos, cov, size = N//2)
-    Y_pos = np.zeros(N//2) + 1
-
-    # Exemplos negativos
-    X_neg = np.random.multivariate_normal(mean_neg, cov, size = N-N//2)
-    Y_neg = np.zeros(N-N//2) - 1
-
-    # Dataset completo
-    X = np.concatenate([X_pos, X_neg], axis = 0)
-    Y = np.concatenate([Y_pos, Y_neg], axis = 0)
-
-    # Embaralha os dados
-    perm = np.random.permutation(N)
-    X = X[perm]
-    Y = Y[perm]
-
-    # Visualização dos dados para comparação com a regressão logística
-    f, ax = plt.subplots(1,2)
-    f.set_size_inches(8,8)
-    f.subplots_adjust(hspace=20.0)
-    ax[0].scatter(X[:,0], X[:,1], c=['green' if y > 0 else 'orange' for y in Y])
-    ax[0].set_title('Estimativa esperada')
-
-    # Regressão logística e visualização da classificação
-    w = logistic_fit(X, Y)
-    print(f'The predicted weights were: ' + str(w))
-    colors = ['blue' if logistic_predict(X[i],w) < 0.5 else 'red' for i in range(X.shape[0])]
-    ax[1].scatter(X[:,0], X[:,1], color=colors)
-    ax[1].set_title('Regressão Logística')
-    plt.show()
-
-def run_tests2():
-
-    N = 1000
-
-    mean_pos = [0, 0, 0, 0]
-    mean_neg = [4, 0, 1, 0]
-    # Calculando a covariancia com variância fixa em 0.5
-    cov = np.dot(.5, np.eye(4,4))
-
-    # Exemplos positivos
-    X_pos = np.random.multivariate_normal(mean_pos, cov, size = N//2)
-    Y_pos = np.zeros(N//2) + 1
-
-    # Exemplos negativos
-    X_neg = np.random.multivariate_normal(mean_neg, cov, size = N-N//2)
-    Y_neg = np.zeros(N-N//2) - 1
-
-    # Dataset completo
-    X = np.concatenate([X_pos, X_neg], axis = 0)
-    Y = np.concatenate([Y_pos, Y_neg], axis = 0)
-
-    # Embaralha os dados
-    perm = np.random.permutation(N)
-    X = X[perm]
-    Y = Y[perm]
-
-    w = logistic_fit(X, Y)
-    print(w)
-
-
-
-if __name__ == '__main__':
-    run_tests1()
-    run_tests2()
+    p = []
+    Xe = np.hstack((np.ones((X.shape[0],1)),X))
+    for i in range(Xe.shape[0]):
+        s = np.dot(w, Xe[i])
+        p.append(1.0 / (1.0 + np.exp(-s)))
+    return p
